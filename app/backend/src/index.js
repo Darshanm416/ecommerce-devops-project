640c7517d2b21a9df818cfb555ce02f3b0a6bdfb
Darshanm416 <ubuntu@ip-172-31-87-39.ec2.internal>
2025-05-30 09:29:07 +0000

    main index code added

---
+++ app/backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const productRoutes = require('./routes/product');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect('mongodb://mongo:27017/ecomdb')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.use('/api/products', productRoutes);
+
+app.listen(5000, () => {
+  console.log('Backend running on port 5000');
+});
