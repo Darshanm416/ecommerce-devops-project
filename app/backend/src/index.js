4830b16542d516e9a41775073ac3409660046121
Darshanm416 <ubuntu@ip-172-31-87-39.ec2.internal>
2025-05-30 18:01:08 +0000

    fix

--- app/backend/src/index.js
+++ app/backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const productRoutes = require('./routes/product');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
