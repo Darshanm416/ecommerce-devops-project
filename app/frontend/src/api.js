569c33b0fe2c81110da50acf76808879759c2837
Darshanm416 <ubuntu@ip-172-31-87-39.ec2.internal>
2025-05-30 17:47:41 +0000

    url updated

--- app/frontend/src/api.js
+++ app/frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://<EC2-PUBLIC-IP>:5000/api',
+  baseURL: 'http://54.234.255.123/:5000/api',
 });
 
 export const fetchProducts = () => API.get('/products');
