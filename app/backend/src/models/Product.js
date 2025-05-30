f3569628fef19ed5944e62dbb9ed665f75e0f334
Darshanm416 <ubuntu@ip-172-31-87-39.ec2.internal>
2025-05-30 11:34:07 +0000

    updtaed

--- app/backend/src/models/Product.js
+++ app/backend/src/models/Product.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
   name: String,
   price: Number,
-  description: String
+  description: String,
+  stock: Number,
 });
 
 module.exports = mongoose.model('Product', productSchema);
