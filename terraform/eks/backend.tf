a0f1004fd20b977aad2cd409df4caa5765995924
Darshanm416 <ubuntu@ip-172-31-87-39.ec2.internal>
2025-06-01 07:25:36 +0000

    backend.tf updated

---
+++ terraform/eks/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "ecommerce-terraform-state-2025"
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "ecommerce-terraform-locks"
+    encrypt        = true
+  }
+}
+
