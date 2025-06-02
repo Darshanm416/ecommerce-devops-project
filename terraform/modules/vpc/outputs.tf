951f6b394720994470672c5fa431be08a8ad4839
Darshanm416 <ubuntu@ip-172-31-87-39.ec2.internal>
2025-06-02 12:36:54 +0000

    upd

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -9,3 +9,10 @@ output "public_subnets" {
 output "private_subnets" {
   value = aws_subnet.private[*].id
 }
+
+# modules/vpc/outputs.tf
+
+output "public_subnet_ids" {
+  description = "List of IDs of the public subnets"
+  value       = aws_subnet.public[*].id # This exports the IDs of all public subnets created by the module
+}
