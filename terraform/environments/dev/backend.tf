31912b3383295ba5dc0bb06209d45455ec79f881
Darshanm416 <ubuntu@ip-172-31-87-39.ec2.internal>
2025-06-02 07:06:22 +0000

    backend.tf

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ecommerce-terraform-state"
+    bucket         = "ecommerce-terraform-state-2025"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "ecommerce-terraform-lock"
