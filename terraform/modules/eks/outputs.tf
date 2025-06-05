e35cb9c8c6521d0f840d2ea9e914f83cea121d5b
Darshanm416 <ubuntu@ip-172-31-87-39.ec2.internal>
2025-06-05 17:54:15 +0000

    up

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -1,7 +1,20 @@
 output "cluster_id" {
-  value = module.eks.cluster_id
+  description = "The ID of the EKS cluster."
+  value       = module.eks_cluster_core.cluster_id # Corrected from module.eks
 }
 
 output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  description = "Endpoint for EKS control plane."
+  value       = module.eks_cluster_core.cluster_endpoint # Corrected from module.eks
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with your cluster."
+  value       = module.eks_cluster_core.cluster_certificate_authority_data # Corrected from module.eks
+}
+
+output "eks_managed_node_groups_default_iam_role_arn" {
+  description = "The ARN of the default EKS managed node group IAM role."
+  # This path is correct as it refers to the output of the inner module call
+  value       = module.eks_cluster_core.eks_managed_node_groups["default"].iam_role_arn
 }
