73fec85153cde134a672c59c5c323580de159a88
Darshanm416 <ubuntu@ip-172-31-87-39.ec2.internal>
2025-06-06 11:49:14 +0000

    up

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -9,3 +9,11 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
+
+output "private_route_table_ids" {
+  value = [for rt in aws_route_table.private_rt : rt.id]
+}
+
+output "public_route_table_ids" {
+  value = [for rt in aws_route_table.public_rt : rt.id]
+}
